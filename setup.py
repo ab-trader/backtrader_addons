1fdc96837d24b39c36f51475c940958ddced067b
ab-trader <xxx@gmail.com>
2019-04-10 21:05:42 -0700

    configured setup py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='backtrader_addons',
+    version='0.0.0',
+    description='Addons (analyzers, observers, indicators etc) for backtrader',
+    url='https://github.com/ab-trader/backtrader_addons',
+    author='ab-trader',
+    author_email='',
+    license='GPLv3+',
+    packages=['backtrader_addons'],
+	install_requires=[ 'backtrader',
+	                 ],
+    zip_safe=False)
